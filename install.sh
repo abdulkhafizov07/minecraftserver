3e4a1a56628732b9fd5e69467104404e1eedc65f
M-chan <new.jasjan.programmer@gmail.com>
2025-10-19 20:59:02 +0500

    chore: adding install.sh to download minecraft server

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+set -e
+
+echo "Downloading paper server version 1.21.8"
+curl https://fill-data.papermc.io/v1/objects/8de7c52c3b02403503d16fac58003f1efef7dd7a0256786843927fa92ee57f1e/paper-1.21.8-60.jar -o server.jar
+
+
