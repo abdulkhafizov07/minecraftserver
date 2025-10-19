69a6646b748eaeb8ec0be0a9b290f0f468c0f090
M-chan <new.jasjan.programmer@gmail.com>
2025-10-19 21:21:41 +0500

    chore: adding some download mods

--- download.sh
+++ download.sh
@@ -1,4 +1,29 @@
-#!/bin/bash
+:q#!/bin/bash
 
-echo "Download "
+set -x
+set -e
+
+echo "Downloading EssentialsX"
+curl https://cdn.modrinth.com/data/hXiIvTyT/versions/Oa9ZDzZq/EssentialsX-2.21.2.jar -o plugins/EssentialsX.jar
+
+echo "Downloading LuckPerms"
+curl https://cdn.modrinth.com/data/Vebnzrzj/versions/OrIs0S6b/LuckPerms-Bukkit-5.5.17.jar -o plugins/LuckPerms.jar
+
+echo "Downloading Vault"
+curl https://cdn.modrinth.com/data/ayRaM8J7/versions/O4gQ7KOy/VaultUnlocked-2.16.0.jar -o plugins/VaultUnlocked.jar
+
+echo "Downloading CoreProtect"
+curl https://cdn.modrinth.com/data/Lu3KuzdV/versions/anOhDobp/CoreProtect-CE-23.0.jar -o plugins/CoreProtect.jar
+
+echo "Downloading ClearLag"
+curl https://cdn.modrinth.com/data/7bhTp28M/versions/5L4jEdUT/ClearLag%201.8%20VER%201.18-1.21.jar -o plugins/ClearLag.jar
+
+echo "Downloading GriefPrevention"
+curl https://cdn.modrinth.com/data/O4o4mKaq/versions/iaJtn30B/GriefPrevention.jar -o plugins/GriefPrevention.jar
+
+echo "Downloading Multiverse"
+curl https://cdn.modrinth.com/data/3wmN97b8/versions/OmRzuPrC/multiverse-core-5.3.2.jar -o plugins/multiverse.jar
+
+echo "Downloading BetterRPT"
+cp spigot/betterrtp.jar plugins/betterrtp.jar
 
